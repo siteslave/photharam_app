78d6f89bf6c562bcd7e8b6dab77041d993501095
Satit Riapit <siteslave@msn.com>
2018-11-14 11:33:01 +0700

    update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,97 +14,102 @@ class _LoginPageState extends State<LoginPage> {
           Container(
             color: Colors.teal,
           ),
-          Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 80.0,
-                ),
-                Image.asset(
-                  'assets/images/moph_logo.png',
-                  width: 150.0,
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'ผลตรวจสุขภาพประจำปี',
-                  style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                Text(
-                  'โรงพยาบาลโพธาราม',
-                  style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Form(
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 0.0)),
-                              labelText: 'เลขบัตรประชาชน',
-                              prefixIcon: Icon(Icons.credit_card)),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(width: 0.0)),
-                              labelText: 'วันเกิด',
-                              prefixIcon: Icon(Icons.calendar_today)),
-                        ),
-                      ],
+          ListView(
+            children: <Widget>[
+              Center(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 80.0,
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: FlatButton(
-                          shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0)),
-                          color: Colors.pink,
-                          onPressed: () {},
-                          child: new Container(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 20.0,
-                              horizontal: 20.0,
+                    Image.asset(
+                      'assets/images/moph_logo.png',
+                      width: 150.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'ผลตรวจสุขภาพประจำปี',
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      'โรงพยาบาลโพธาราม',
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide(width: 0.0)),
+                                  labelText: 'เลขบัตรประชาชน',
+                                  prefixIcon: Icon(Icons.credit_card)),
                             ),
-                            child: Text(
-                              "เข้าใช้งานแอปพลิเคชัน",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontSize: 20.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
+                            SizedBox(
+                              height: 10.0,
                             ),
-                          ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide(width: 0.0)),
+                                  labelText: 'วันเกิด',
+                                  prefixIcon: Icon(Icons.calendar_today)),
+                            ),
+                          ],
                         ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: FlatButton(
+                              shape: new RoundedRectangleBorder(
+                                  borderRadius:
+                                      new BorderRadius.circular(30.0)),
+                              color: Colors.pink,
+                              onPressed: () {},
+                              child: new Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 20.0,
+                                  horizontal: 20.0,
+                                ),
+                                child: Text(
+                                  "เข้าใช้งานแอปพลิเคชัน",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           )
         ],
       ),
